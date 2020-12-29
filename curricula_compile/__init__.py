145e3d0c2bfca531af30218843ac39ccebe61f2b
Noah Kim <noahbkim@gmail.com>
2020-12-28 23:49:08 -0500

    Clean up to be package

--- curricula_compile/__init__.py
+++ curricula_compile/__init__.py
@@ -5,6 +5,8 @@ from curricula.log import log
 
 from .compilation import CurriculaTarget, Configuration
 
+__version__ = "2.0.0"
+
 
 @timed("compile", printer=log.info)
 def compile(assignment_path: Path, artifacts_path: Path, custom_template_path: Path = None, options: dict = None):
